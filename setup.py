6945f1d3412560cb9e90281f266f21a23cd0215a
Rekhansh Raje Deshmukh <rekhanshdeshmukh@gmail.com>
2025-05-21 18:51:44 +0530

    component

---
+++ setup.py
@@ -0,0 +1,27 @@
+## Help to create a package
+
+from setuptools import setup, find_packages
+from typing import List
+
+
+HYPEN_E_DOT ='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    # This function will return the list of requirements 
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("/n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+
+setup(
+    name='Legendary-lamp',
+    version='0.0.1',
+    author='Rekhansh',
+    author_email='rekhanshdeshmukh@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
